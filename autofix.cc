ca9f5782fed21386b005d41ca0cfb5647982b450
microsvuln <55649192+Microsvuln@users.noreply.github.com>
2021-09-10 21:44:35 +0430

    Initialize the autofix pass
    Initialize the autofix pass

---
+++ autofix.cc
@@ -0,0 +1,41 @@
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Passes/PassBuilder.h"
+
+using namespace llvm;
+
+
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getInjectFuncCallPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "inject-func-call", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, ModulePassManager &MPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "inject-func-call") {
+                    MPM.addPass(InjectFuncCall());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getInjectFuncCallPluginInfo();
+}
+
+//-----------------------------------------------------------------------------
+// Legacy PM Registration
+//-----------------------------------------------------------------------------
+char LegacyInjectFuncCall::ID = 0;
+
+// Register the pass - required for (among others) opt
+static RegisterPass<LegacyInjectFuncCall>
+    X(/*PassArg=*/"legacy-autofix-mem-leaks", /*Name=*/"autofixmemleaks",
+      /*CFGOnly=*/false, /*is_analysis=*/false);

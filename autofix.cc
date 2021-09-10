da2ca1ccb75a78cbea4024afdf4d618f1045fc07
microsvuln <55649192+Microsvuln@users.noreply.github.com>
2021-09-10 22:06:23 +0430

    Update with new headers

--- autofix.cc
+++ autofix.cc
@@ -1,12 +1,21 @@
 
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/Passes/PassPlugin.h>
+#include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/InstIterator.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/ADT/Statistic.h>
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Type.h>
 #include <llvm/Pass.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/Casting.h>
+#include <llvm/Support/Compiler.h>
 #include <llvm/Support/raw_ostream.h>
 
 using namespace llvm;

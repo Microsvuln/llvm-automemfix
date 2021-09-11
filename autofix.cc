021d3f9e91acdb72852a1ca87d489bb6719a35a2
microsvuln <55649192+Microsvuln@users.noreply.github.com>
2021-09-11 21:12:41 +0430

    Some updates
    Some updates

--- autofix.cc
+++ autofix.cc
@@ -1,10 +1,10 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/IR/InstrTypes.h"
-#include "llvm/IR/IRBuilder.h"
+#include <llvm/Pass.h>
+#include <llvm/IR/Function.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+#include <llvm/IR/InstrTypes.h>
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/Analysis/ValueTracking.h>
 using namespace llvm;
 
@@ -21,23 +21,27 @@ namespace {
         for (Instruction &I: B) {
             if(CallInst* call_inst = dyn_cast<CallInst>(&I)) {
                 bool isMalloc = true;
+                bool isFree = true;
                 Function *Callee = call_inst->getCalledFunction();
                 if(!Callee) continue;
                         if(call_inst->getCallingConv() != llvm::CallingConv::C) continue;
                         std::string FuncName = Callee->getName().str() ;
-                        isMalloc &= (!FuncName.compare("malloc"));       
+                        isMalloc &= (!FuncName.compare("malloc"));
+                        isFree   &= (!FuncName.compare("free"));
                         if(isMalloc == true){
-                            errs() << "\nWe have malloc() calls :D";
-
+                            errs() << "\nWe have malloc() calls\n";
                         }
-                        else{
-                            errs() << "\nWe have no malloc calls()";
+                        if(isFree == true)
+                        {
+                                errs() << "\nWe have free() calls\n";
                         }
-                    Value *str1Pointer = call_inst->getArgOperand(0);
-                    std::string Str1, Str2;
-                    StringRef TmpStr;
-                    getConstantStringInfo(str1Pointer, TmpStr);
+                    
+                    ///// Value *str1Pointer = call_inst->getArgOperand(0);
+                    ////// std::string Str1, Str2;
+                    ///// StringRef TmpStr;
+                    ///// getConstantStringInfo(str1Pointer, TmpStr);
                     errs() << call_inst->getArgOperand(0);
+                    errs() << "\n";
             }
 
 
